2ed1a6435b6af77d055ef8fce3a575b3ec22ea1a
PreethaPinto <preethacp@gmail.com>
2023-03-08 22:43:19 +1000

    Bracket notation for strings, Arrat basics, Array methods - push, pop, shift, unshift.

---
+++ 041. nestingArrays.js
@@ -0,0 +1,5 @@
+// Only change code below this line
+const myArray = [
+  ["cookies", 10],
+  ["cream", 5],
+];
---
+++ 049. shoppingList.js
@@ -0,0 +1,7 @@
+const myList = [
+  ["apples", 10],
+  ["bananas", 12],
+  ["cucumbers", 3],
+  ["capsicum", 2],
+  ["onions", 6],
+];
