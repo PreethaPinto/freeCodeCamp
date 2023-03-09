928fd11862a36e51f3f284047fd2de41570538c6
PreethaPinto <preethacp@gmail.com>
2023-03-09 22:38:55 +1000

    Objects - accessing, adding and deleting properties, nested arrays and nested objects

---
+++ 1. Basic-Javascript/083. buildingObjects.js
@@ -0,0 +1,9 @@
+const myDog = {
+  // Only change code below this line
+  name: "Milo",
+  legs: 4,
+  tails: 1,
+  friends: ["Ollie", "Bruno"],
+
+  // Only change code above this line
+};
---
+++ 1. Basic-Javascript/092. manipulatingComplexArrays.js
@@ -0,0 +1,16 @@
+const myMusic = [
+  {
+    artist: "Billy Joel",
+    title: "Piano Man",
+    release_year: 1973,
+    formats: ["CD", "8T", "LP"],
+    gold: true,
+  },
+  {
+    artist: "Abba",
+    title: "Abba",
+    release_year: 1970,
+    formats: ["CD", "8T", "LP"],
+    gold: true,
+  },
+];
